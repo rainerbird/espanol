99b88046b3274ecf149f732a20954ed3d545cea1
William Rainbird <wrainbird@gmail.com>
2022-08-29 16:13:32 +1000

    added colors to sanity

---
+++ sanity/schemas/color.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'color',
+  type: 'document',
+	title: 'Color',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'translation',
+      type: 'string',
+      title: 'Translation'
+    }
+  ]
+}
